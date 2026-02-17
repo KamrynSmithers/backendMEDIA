3095bdfe5c76bff88105a439fc8bde43466b68f3
KamrynSmithers <kamrynaliecesmithers@gmail.com>
2026-02-17 15:59:59 -0500

    password reset fields to user schema

--- models/User.js
+++ models/User.js
@@ -4,6 +4,8 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  resetPasswordToken: String, 
+  resetPasswordExpires: Date,  
 }, { timestamps: true });
 
 export default mongoose.model("User", userSchema);
