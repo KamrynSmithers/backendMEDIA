4a7f65bda8ec4a6d3b72443f96873d4a4d2ba9d2
KamrynSmithers <kamrynaliecesmithers@gmail.com>
2026-02-17 15:47:39 -0500

    feat: add forgot password handler

--- controllers/passwordController.js
+++ controllers/passwordController.js
@@ -1,3 +1,14 @@
 import User from "../models/User.js";
 import nodemailer from "nodemailer";
 import crypto from "crypto";
+
+// Send reset email
+export const forgotPassword = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    
