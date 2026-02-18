6287c5a5eca21348326c6326f183a48fc4b44208
KamrynSmithers <kamrynaliecesmithers@gmail.com>
2026-02-18 04:47:50 -0500

    validate media comment input fields'

--- controllers/mediaCommentController.js
+++ controllers/mediaCommentController.js
@@ -1,3 +1,10 @@
 import MediaComment from "../models/MediaComment.js";
 
-export const createMediaComment = async (req, res) => {}
+export const createMediaComment = async (req, res) => {
+    try {
+    const { contentTitle, contentType, text, rating } = req.body;
+    const userId = req.user.id;
+
+    if (!contentTitle || !contentType || !text) {
+      return res.status(400).json({ message: "Content title, type, and text required" });
+    }
