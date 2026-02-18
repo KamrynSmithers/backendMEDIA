b5ae6cfebabf9d81ae615fd8acb28a6404852d9c
KamrynSmithers <kamrynaliecesmithers@gmail.com>
2026-02-18 04:44:14 -0500

    index contentTitle and contentType fields' '

--- models/MediaComment.js
+++ models/MediaComment.js
@@ -2,5 +2,10 @@ import mongoose from "mongoose";
 const mediaCommentSchema = new mongoose.Schema ({
     contentTitle: { type: String, required: true },
     contentType: { type: String, enum: ['movie', 'book', 'music'], required: true },
-    
-})
+      user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    text: { type: String, required: true },
+    rating: { type: Number, min: 1, max: 5 },
+    }, { timestamps: true });
+
+    mediaCommentSchema.index({ contentTitle: 1, contentType: 1 });
+    export default mongoose.model("MediaComment", mediaCommentSchema);
